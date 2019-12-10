72499088b5650383d3e72b709e972da093d5851f
Femin James <47390278+femin004@users.noreply.github.com>
2019-12-10 20:40:14 +0530

    My first program

---
+++ first-app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace first_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("first program!");
+
+            Console.WriteLine("\n\npress any key");
+            Console.ReadKey();
+        }
+    }
+}
